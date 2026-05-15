7c7b91c3ffbcc98c2f57475c1f8068b19fa85343
Rannyer <rdesenhos@gmail.com>
2026-05-15 17:35:50 -0300

    primeiro testes no heroku

--- src/test/java/PrimeiroTesteSelenium.java
+++ src/test/java/PrimeiroTesteSelenium.java
@@ -1,28 +1,51 @@
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.plaf.TableHeaderUI;
 import java.io.FilterOutputStream;
 
 public class PrimeiroTesteSelenium {
 
-    @Test
-    void deveAbrirSiteCapgemini() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-
-
-        driver.get("https://www.capgemini.com/br-pt/");
-
-        WebElement element = driver.findElement(By.xpath("//a[@class='analytics-hero-banner-title']"));
-        System.out.println(element.getText());
-        element.click();
-        Thread.sleep(5000); // Espera 5 segundos para a página carregar completamente
-
-        System.out.println(driver.getTitle());
-
+    private WebDriver driver;
+    @BeforeEach
+    void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterEach
+    void close(){
         driver.quit();
     }
+
+    @Test
+    void deveTestarLoginComSucesso() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement buttonLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword!");
+        buttonLogin.click();
+
+        WebElement mensagem = driver.findElement(By.id("flash"));
+
+
+        assertTrue(mensagem.getText().contains("You logged into a secure area!"));
+        assertTrue(driver.getCurrentUrl().contains("/secure"));
+
+    }
+
+
+
+
+
 }
